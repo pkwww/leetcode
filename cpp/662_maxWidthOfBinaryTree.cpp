7ebc4321af6b50cff4e04e472939f38a4bb32741
pkw <pkwww.2000@gmail.com>
2021-10-11 02:54:55 +0800

    662

--- cpp/662_maxWidthOfBinaryTree.cpp
+++ cpp/662_maxWidthOfBinaryTree.cpp
@@ -11,8 +11,52 @@ struct TreeNode {
 
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-        if (root == NULL)
+    int widthOfBTArray(const vector<TreeNode*>& nodes, int max_height) {
+        int max_width = 1;
+        for (int h = 0; h < max_height; h++) {
+            int start_i = (1 << h) - 1;
+            int end_i = (1 << (h+1)) - 1;
+            int start = end_i;
+            int end = start_i;
+            for (int i = start_i; i < end_i; i++) {
+                if (nodes[i] != nullptr) {
+                    start = i;
+                    break;
+                }
+            }
+            for (int i = end_i-1; i >= start_i; i--) {
+                if (nodes[i] != nullptr) {
+                    end = i;
+                    break;
+                }
+            }
+            int level_width = end - start + 1;
+            max_width = max(level_width, max_width);
+        }
+        return max_width;
+    }
+    int treeHeight(TreeNode* root) {
+        if (root == nullptr)
             return 0;
+        return 1 + max(treeHeight(root->left), treeHeight(root->right));
+    }
+    void build(vector<TreeNode*>& nodes, TreeNode* root, int i) {
+        if (root == nullptr) {
+            return;
+        }
+        nodes[i] = root;
+        build(nodes, root->left, 2*i+1);
+        build(nodes, root->right, 2*i+2);
+    }
+    
+    int bfs(TreeNode* root) {
+        
+    }
+    
+    int widthOfBinaryTree(TreeNode* root) {
+        // int height = treeHeight(root);
+        // vector<TreeNode*> nodes((1 << height) - 1, nullptr);
+        // build(nodes, root, 0);
+        // return widthOfBTArray(nodes, height);
     }
 };
